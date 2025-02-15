82aa34a61e8a49bfbd0549f6add29a49424fa109
Chayapol Chaipongsawalee <chayapol.c@arise.tech>
2025-02-15 23:40:19 +0700

    test: list comment

--- src/components/ListComponent/ListComment.test.tsx
+++ src/components/ListComponent/ListComment.test.tsx
@@ -1,6 +1,8 @@
-import { describe, it, expect, test } from 'vitest';
+import { describe, it, expect, test, vi } from 'vitest';
 import { render } from '@testing-library/react';
 import ListComment from './ListComment';
+import IUser from '../../types/user';
+import IComment from '../../types/comment';
 
 test('demo', () => {
   expect(true).toBe(true);
@@ -8,7 +10,24 @@ test('demo', () => {
 
 describe('render', () => {
   it('renders the main page', () => {
-    render(<ListComment />);
+    const mockCurrentUser: IUser = {
+      image: {
+        png: '',
+        webp: '',
+      },
+      username: '',
+    };
+    const mockItemList: IComment[] = [];
+
+    render(
+      <ListComment
+        itemList={mockItemList}
+        currentUser={mockCurrentUser}
+        onClickReply={vi.fn()}
+        onIncreaseScore={vi.fn()}
+        onDecreaseScore={vi.fn()}
+      />
+    );
     expect(true).toBeTruthy();
   });
 });
