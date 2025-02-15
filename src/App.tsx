54d81b7f32a1b5b86951773c3712e584d3214e01
Chayapol Chaipongsawalee <chayapol.c@arise.tech>
2025-02-15 23:34:53 +0700

    feat: impl component

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,49 @@
-import './App.css'
+import './App.css';
+import data from './data.json';
+import ListComment from './components/ListComponent/ListComment';
+import IComment from './types/comment';
+import { useCallback, useMemo } from 'react';
+import IUser from './types/user';
+import CommentInput from './components/CommentInput/CommentInput';
 
 function App() {
+  const currentUser = useMemo<IUser>(() => data.currentUser, []);
+  const commentList = useMemo<IComment[]>(() => data.comments, []);
+
+  const handleClickReply = useCallback((id: number) => {
+    console.log('reply', id);
+  }, []);
+  const handleIncreaseScore = useCallback(() => {
+    console.log('increase');
+  }, []);
+  const handleDecreaseScore = useCallback(() => {
+    console.log('decrease');
+  }, []);
+  const handleSendComment = useCallback(() => {}, []);
+  const handleEditComment = useCallback(() => {}, []);
+  const handleDeleteComment = useCallback(() => {}, []);
 
   return (
     <>
-      <header>
-
-      </header>
+      <header></header>
       <main>
-
+        <ListComment
+          itemList={commentList}
+          currentUser={currentUser}
+          onClickReply={handleClickReply}
+          onDecreaseScore={handleDecreaseScore}
+          onIncreaseScore={handleIncreaseScore}
+          onDeleteComment={handleEditComment}
+          onEditComment={handleDeleteComment}
+        />
+        <CommentInput
+          currentUser={currentUser}
+          onSendComment={handleSendComment}
+        />
       </main>
-      <footer>
-
-      </footer>
+      <footer></footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
